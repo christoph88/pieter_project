14437f855c00a165358b3cfd44f14a7191a57e8f
Christoph Geypen <geypenchristoph@gmail.com>
2016-06-30 14:38:35 +0200

    added sls formula to frontend

--- 3dprintincludesjs3dprint-frontend.js
+++ 3dprintincludesjs3dprint-frontend.js
@@ -1164,14 +1164,66 @@ function p3dCalculatePrintingCost( product_info ) {
               printing_vol = product_info['model']['box_volume'] * price;
           }
         }
-      printing_cost=50.50 + printing_vol*printer.data('price');
+      printing_cost = 50.50 + printing_vol * printer.data('price');
 		}
 		else if ( printer.data('price_type')=="sls" ) {
 			//printing_cost=product_info['model']['weight']*printer.data('price')*1000000000000;
       // added dimensions
-      product_info['model']['x_dim']
-      //product_info['model']['y_dim']
-      //product_info['model']['z_dim']
+
+      var x = product_info['model']['x_dim'];
+      var y = product_info['model']['y_dim'];
+      var z = product_info['model']['z_dim'];
+      var xyz = x * y * z;
+
+      var calcSLS = function(multiplier) {
+        if (z * z< x * y) {
+            printing_cost = (3.14 + ( 0.0023 * x * z))+ ( (0.042 * (( x*y)/(x*10))) * (z-1)) * multiplier
+        } else
+        if (x < z) {
+            printing_cost = (3.14 + ( 0.0023 * x * z))+ ( (0.042 * (( z*y)/(z*10))) * (x-1)) * multiplier
+        } else
+        {
+            printing_cost = (3.14 + ( 0.0023 * x * y))+ ( (0.042 * (( x*z)/(x*10))) * (y-1)) * multiplier
+        }
+      };
+
+
+      //if smaller than x use following multiplier
+      //use . for a comma
+      //use : to split x and multiplier
+      //use ; to define end of multiplier and start a new comparison
+      var multiplierString = `
+      1001:-0.3;
+      8001:0.3;
+      27001:0.5;
+      64001:0.62;
+      125001:0.8;
+      216001:0.9;
+      343001:1.05;
+      512001:1.2;
+      729001:1.32;
+      1000001:1.47;
+      1331001:1.75;
+      2197001:1.9;
+      2744001:2.05;
+      3375001:2.15;
+      4096001:2.3;
+      4913001:2.45;
+      5832001:2.6;
+      6859001:2.75;
+      8000001:2.95;
+      15625001:4;
+      120000001:5;
+      `
+
+      var multiplierArray = multiplierString.split(';');
+      for (var i = 0; i < multiplierArray.length; i++) {
+        var helper = multiplierArray[i].split(':');
+        if (xyz < helper[0]) {
+          calcSLS(helper[1]);
+          break;
+        }
+      }
 		}
 
 	}
